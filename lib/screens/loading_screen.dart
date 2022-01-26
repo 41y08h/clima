65320b19f735b65de879c2c899f17eb493408635
Piyush <piyush41y08h@gmail.com>
2022-01-26 22:35:36 +0530

    make basic location and weather api calls

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,16 +11,41 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    print("latitide: ${location.latitude} longitude: ${location.longitude}");
+  }
+
+  void getData() async {
+    http.Response response = await http.get(Uri.parse(
+        "https://api.openweathermap.org/data/2.5/weather?q=saharsa&appid=8008bcf0e10d9ad68ab28560b9dc6b49&units=metric"));
+
+    final data = jsonDecode(response.body);
+
+    final String city = data["name"];
+    final double temp = data["main"]["temp"];
+    final int condition = data["weather"][0]["id"];
+
+    print(city);
+    print(temp);
+    print(condition);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
+
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
+        child: Text('The contents of this Scaffold was removed.'),
       ),
     );
   }
